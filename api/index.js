e68da3612ac5d02626185f79cf328719194126b3
PatilVaibhavi-11 <vaibhavipa02@gmail.com>
2024-11-15 15:22:13 +0530

    Add files via upload

---
+++ api/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from"dotenv";
+import dbConnect from "../config/database.js";
+import User from "../models/user.js";
+import router from "../Routes/FeedbackRoutes.js";
+dotenv.config();
+
+const app = express();
+const PORT = 3000;
+app.use(express.json())
+app.use("/api/v1",router)
+dotenv.config
+dbConnect();
+
+
+
+
+
+
+
+app.listen(PORT,()=>{
+    console.log(`server is running at PORT : ${PORT}`)
+})
